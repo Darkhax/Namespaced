6c4bae8612296c43190066c946ef53a486272e7a
Tyler Hancock <darklime@live.ca>
2019-09-05 01:49:18 -0600

    Add package info.

---
+++ src/main/java/net/darkhax/namespaced/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains the basic classes needed for namespaced identifiers and an optional
+ * registry which uses them.
+ */
+@javax.annotation.ParametersAreNonnullByDefault
+package net.darkhax.namespaced;
